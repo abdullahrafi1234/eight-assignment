7aa0a8f349550723eb806119decc3175981f4f2e
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-03-28 05:12:04 +0600

    home page done

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,9 +1,37 @@
+import { CiStar } from "react-icons/ci";
 
+const Book = ({ book }) => {
 
-const Book = () => {
+    const { author, boodId, bookName, category, image, tags, rating} = book;
+
     return (
         <div>
-                <h2>books</h2>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <div className="bg-[#F3F3F3] px-20 py-12 rounded-xl">
+                        <img src={image} alt="Shoes" className="rounded-xl" />
+                    </div>
+                </figure>
+
+                <div className="pl-14 flex pt-8 gap-8">
+                    <p className="text-[#23BE0A] px-6 py-2 font-medium bg-[#23BE0A0D] rounded-xl">{tags[0]}</p>
+                    <p className="text-[#23BE0A] px-6 py-2 font-medium bg-[#23BE0A0D] rounded-xl">{tags[1]}</p>
+
+                </div>
+                <div className="card-body  pl-14">
+                    <h2 className="card-title font-bold">{bookName}</h2>
+                    <p className="font-medium">By: {author}</p>
+                    <div className=" my-4 border border-dotted border-gray-300">
+                      
+                    </div>
+                </div>
+
+                <div className="flex pl-14  pr-14 justify-between pb-8 font-medium">
+                    <p className="">{category}</p>
+
+                    <p className="flex gap-2 items-center">{rating}<CiStar className="text-xl"></CiStar></p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -2,7 +2,18 @@ import { useEffect, useState } from "react";
 import Book from "../Book/Book";
 
 
-const Books = () => {
+const Books = () => {<div className="card w-96 bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
 
     const [books , setBooks] = useState([])
     console.log(books)
