0f9728731d2255bfae331608c035f72ea88f55ad
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-03-27 14:22:11 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div className='text-7xl'>hello world
+    <button className="btn btn-secondary">Secondary</button></div>
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
